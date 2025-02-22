db1c8a40cc6abe7c6d318b8da79c00d7b7ccb80a
KareemMusleh <prototribe@protonmail.com>
2025-02-22 14:19:22 +0700

    fixed unslothvisiondatacollator

--- unsloth_zoo/vision_utils.py
+++ unsloth_zoo/vision_utils.py
@@ -279,7 +279,7 @@ class UnslothVisionDataCollator:
             else:
                 image, video = process_vision_info(messages)
             texts .append(message)
-            images.append(image)
+            if image: images.append(image)
         pass
 
         # Tokenize the texts and process the images
