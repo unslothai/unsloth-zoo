95bd373c76b7bd42e81f649c6796d444cdde5da8
Daniel Han <danielhanchen@gmail.com>
2025-05-28 13:58:31 -0700

    Update gemma.py

--- unsloth_zoo/temporary_patches/gemma.py
+++ unsloth_zoo/temporary_patches/gemma.py
@@ -156,7 +156,6 @@ pass
 TEMPORARY_PATCHES.append(patch_Gemma3Processor)
 
 def patch_Gemma3ForConditionalGeneration():
-    print("Patching patch_Gemma3ForConditionalGeneration")
     try:
         import transformers.models.gemma3.modeling_gemma3
     except:
