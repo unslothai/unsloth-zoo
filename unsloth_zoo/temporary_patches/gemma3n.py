5ad19dee1d55cb901cc45bc590352c53602056a3
Daniel Han <danielhanchen@gmail.com>
2025-06-26 07:55:46 -0700

    Update gemma3n.py

--- unsloth_zoo/temporary_patches/gemma3n.py
+++ unsloth_zoo/temporary_patches/gemma3n.py
@@ -127,7 +127,7 @@ def patch_Gemma3nForConditionalGeneration_forward():
             print("Unsloth: Failed to patch Gemma3nForConditionalGeneration.")
     else:
         forward = torch.compile(forward, fullgraph = True, dynamic = True, options = torch_compile_options)
-        transformers.gemma3n.gemma3n.modeling_gemma3n.Gemma3nForConditionalGeneration.forward = forward
+        transformers.models.gemma3n.modeling_gemma3n.Gemma3nForConditionalGeneration.forward = forward
     return
 pass
 TEMPORARY_PATCHES.append(patch_Gemma3nForConditionalGeneration)
