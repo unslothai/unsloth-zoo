fcdcd62aaac6d7dfe5a657b76ebe8cacefa7d09c
Daniel Han <danielhanchen@gmail.com>
2025-06-30 19:04:21 -0700

    Update vllm_utils.py

--- unsloth_zoo/vllm_utils.py
+++ unsloth_zoo/vllm_utils.py
@@ -169,7 +169,6 @@ if importlib.util.find_spec("vllm") is not None:
                               out_dim_1,
                               dtype=inference_dtype,
                               device=x.device)
-            print("A", out.shape)
             apply_bnb_4bit(bf_x, qweight, offsets, out)
             out = out.to(original_type)
 
