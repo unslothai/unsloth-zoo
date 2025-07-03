55349dc86b77093daea5eaffea17a296d18e704c
majing <jing1.ma@intel.com>
2025-07-03 09:06:39 +0800

    Remove hardcode compile_threads number in unsloth_compile_transformers (#187)
    Signed-off-by: majing <Jing1.Ma@intel.com>

--- unsloth_zoo/compiler.py
+++ unsloth_zoo/compiler.py
@@ -1636,7 +1636,6 @@ def unsloth_compile_transformers(
         "memory_planning"           : True,
         "coordinate_descent_tuning" : UNSLOTH_COMPILE_MAXIMUM,
         "trace.graph_diagram"       : UNSLOTH_COMPILE_DEBUG or debug,
-        "compile_threads"           : 24,
         "combo_kernels"             : False, # Causes incompatible gradient sizes on 2.6
         "group_fusion"              : True,
         "disable_progress"          : not UNSLOTH_ENABLE_LOGGING,
