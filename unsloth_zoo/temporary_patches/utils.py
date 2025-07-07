3bcbf446da120da6d3e9b4a2b306fcebb6edc426
Daniel Han <danielhanchen@gmail.com>
2025-07-07 01:55:52 -0700

    Update utils.py

--- unsloth_zoo/temporary_patches/utils.py
+++ unsloth_zoo/temporary_patches/utils.py
@@ -251,7 +251,7 @@ if TransformersKwargs   != t._TypedDictMeta: TYPE_MAPPINGS[TransformersKwargs]  
 if FlashAttentionKwargs != t._TypedDictMeta: TYPE_MAPPINGS[FlashAttentionKwargs] = t._TypedDictMeta
 if LossKwargs           != t._TypedDictMeta: TYPE_MAPPINGS[LossKwargs]           = t._TypedDictMeta
 
-def canonicalize_annotation(annotation: Any) -> Any:
+def _canonicalize_annotation(annotation: Any) -> Any:
     """
     Canonicalize type annotations for consistent comparison.
     Makes List[int], typing.List[int], list[int] equivalent.
@@ -266,6 +266,15 @@ def canonicalize_annotation(annotation: Any) -> Any:
             args = tuple(canonicalize_annotation(arg) for arg in args)
             return (origin, args)
     return TYPE_MAPPINGS.get(annotation, annotation)
+pass
+def canonicalize_annotation(annotation: Any) -> Any:
+    annotation = _canonicalize_annotation(annotation)
+    # Fix up reordering of Union with sorting
+    # Union[str, List[str], list[str]] gets reduced to Union[str, list[str]]
+    # due to duplicates. We also sort Unions
+    if type(annotation) is tuple and len(annotation) == 2 and annotation[0] == t.Union:
+        annotation = (annotation[0], tuple(set(annotation[1])),)
+    return annotation
 pass
 
 
