351fc57bf5c4a78989d934d4f85b9acb45eb171b
Daniel Han <danielhanchen@gmail.com>
2025-07-07 04:15:43 -0700

    Update compiler.py

--- unsloth_zoo/compiler.py
+++ unsloth_zoo/compiler.py
@@ -1609,9 +1609,9 @@ def unsloth_compile_transformers(
     if hasattr(modeling_file, "__UNSLOTH_PATCHED__"): return
 
     # Use transformers model_type logger to suppress message: Remove `use_cache=True` is incompatible with gradient checkpointing. Setting `use_cache=False`
-    exec("model_logger.addFilter(HideLoggingMessage('`use_cache`'))", globals(), locals())
+    exec("model_logger.addFilter(HideLoggingMessage('use_cache'))", globals(), locals())
     # Use transformers model_type logger to suppress message: RYou have set `compile_config`, but we are unable to meet the criteria for compilation.
-    exec("model_logger.addFilter(HideLoggingMessage('`compile_config`'))", globals(), locals())
+    exec("model_logger.addFilter(HideLoggingMessage('compile_config'))", globals(), locals())
 
     # Instead of Inductor Compilation:
     try:
