fc6b7286c72d316d6fe5c287a61dabb8e2b332a6
Daniel Han <danielhanchen@gmail.com>
2025-07-10 02:10:03 -0700

    Update __init__.py

--- unsloth_zoo/__init__.py
+++ unsloth_zoo/__init__.py
@@ -14,7 +14,7 @@
 # You should have received a copy of the GNU Lesser General Public License
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-__version__ = "2025.7.1"
+__version__ = "2025.7.2"
 
 import os
 # Hugging Face Hub faster downloads
