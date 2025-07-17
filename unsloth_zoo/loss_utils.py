bbba3d56af51dc32749f087b12f465d84161ffc6
Daniel Han <danielhanchen@gmail.com>
2025-07-17 05:40:47 -0700

    Update loss_utils.py

--- unsloth_zoo/loss_utils.py
+++ unsloth_zoo/loss_utils.py
@@ -331,17 +331,19 @@ def _unsloth_get_batch_samples(self, epoch_iterator, num_batches, device = None,
         logger.info(f"Unsloth: num_items_in_batch = {num_items_in_batch}")
     
     # Increment counter and set compiler stance
-    global TRAINING_ITERATIONS
-    if TRAINING_ITERATIONS == 4:
-        # Skip guards after 4 warmup runs
-        torch_compiler_set_stance(stance = "default", skip_guard_eval_unsafe = True)
-        if UNSLOTH_ENABLE_LOGGING:
-            logger.info(f"Unsloth: Skipping torch.compile guards after 4 steps at TRAINING_ITERATIONS = {TRAINING_ITERATIONS}")
-    elif torch_dynamo_eval_frame._stance.skip_guard_eval_unsafe == False and TRAINING_ITERATIONS > 8:
-        # Reset TRAINING_ITERATIONS
-        torch_compiler_set_stance(stance = "default", skip_guard_eval_unsafe = False)
-        TRAINING_ITERATIONS = 0
-    TRAINING_ITERATIONS += 1
+    if not hasattr(self.model, "vllm_engine"):
+        # Only for non vLLM runs! Otherwise errors out
+        global TRAINING_ITERATIONS
+        if TRAINING_ITERATIONS == 4:
+            # Skip guards after 4 warmup runs
+            torch_compiler_set_stance(stance = "default", skip_guard_eval_unsafe = True)
+            if UNSLOTH_ENABLE_LOGGING:
+                logger.info(f"Unsloth: Skipping torch.compile guards after 4 steps at TRAINING_ITERATIONS = {TRAINING_ITERATIONS}")
+        elif torch_dynamo_eval_frame._stance.skip_guard_eval_unsafe == False and TRAINING_ITERATIONS > 8:
+            # Reset TRAINING_ITERATIONS
+            torch_compiler_set_stance(stance = "default", skip_guard_eval_unsafe = False)
+            TRAINING_ITERATIONS = 0
+        TRAINING_ITERATIONS += 1
     return batch_samples, num_items_in_batch
 pass
 
