6ee05230a673bbb7e620873a1c7ac94a031212fa
Daniel Han <danielhanchen@gmail.com>
2025-07-24 02:25:55 -0700

    Update __init__.py

--- unsloth_zoo/__init__.py
+++ unsloth_zoo/__init__.py
@@ -53,11 +53,11 @@ pass
 
 # Triton compile debugging
 if (os.environ.get("UNSLOTH_COMPILE_DEBUG", "0") == "1"):
-    os.environ["TRITON_ENABLE_LLVM_DEBUG"] = "1" # Lots of debugging info
+    os.environ["TRITON_ENABLE_LLVM_DEBUG"] = "0" # Lots of debugging info
     # Can add print statements, but slower so disable
     # Also fails on get_int1_ty for example (bool)
     os.environ["TRITON_INTERPRET"] = "0"
-    os.environ["CUDA_LAUNCH_BLOCKING"] = "1" # Blocking calls for debugging
+    os.environ["CUDA_LAUNCH_BLOCKING"] = "0" # Blocking calls for debugging
 pass
 
 
