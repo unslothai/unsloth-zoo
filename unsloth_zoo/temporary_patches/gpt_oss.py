ae826780d7bf132e7cf388d214197ff84ff75cd7
Daniel Han <danielhanchen@gmail.com>
2025-08-07 09:59:21 -0700

    Update gpt_oss.py

--- unsloth_zoo/temporary_patches/gpt_oss.py
+++ unsloth_zoo/temporary_patches/gpt_oss.py
@@ -445,6 +445,11 @@ class GptOssMLP(nn.Module):
 pass
 
 def patch_gpt_oss_linearized():
+    model_name = os.environ.get("UNSLOTH_MODEL_NAME", "")
+    if "gpt-oss" in model_name and model_name.endswith("-unsloth-bnb-4bit"):
+        pass
+    else:
+        return
     try:
         import transformers.models.gpt_oss.modeling_gpt_oss
     except Exception as e:
