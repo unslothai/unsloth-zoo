6d593d361236166bcc562eb6e8e50606961f5a27
Daniel Han <danielhanchen@gmail.com>
2025-08-22 01:25:52 -0700

    Update gpt_oss.py

--- unsloth_zoo/temporary_patches/gpt_oss.py
+++ unsloth_zoo/temporary_patches/gpt_oss.py
@@ -638,40 +638,41 @@ def patch_GptOssAttention():
         **kwargs: Unpack[TransformersKwargs],
     ) -> tuple[torch.Tensor, torch.Tensor]:
         input_shape = hidden_states.shape[:-1]
-        hidden_states = hidden_states.to(torch.bfloat16)
-        hidden_shape = (*input_shape, -1, self.head_dim)
+        
         with torch.autocast(device_type = "cuda", dtype = torch.bfloat16):
+            hidden_states = hidden_states.to(torch.bfloat16)
+            hidden_shape = (*input_shape, -1, self.head_dim)
             query_states = self.q_proj(hidden_states).view(hidden_shape).transpose(1, 2)
             key_states = self.k_proj(hidden_states).view(hidden_shape).transpose(1, 2)
             value_states = self.v_proj(hidden_states).view(hidden_shape).transpose(1, 2)
 
-        cos, sin = position_embeddings
-        query_states, key_states = apply_rotary_pos_emb(query_states, key_states, cos, sin)
+            cos, sin = position_embeddings
+            query_states, key_states = apply_rotary_pos_emb(query_states, key_states, cos, sin)
 
-        if past_key_values is not None:
-            cache_kwargs = {"cache_position": cache_position}
-            key_states, value_states = past_key_values.update(key_states, value_states, self.layer_idx, cache_kwargs)
+            if past_key_values is not None:
+                cache_kwargs = {"cache_position": cache_position}
+                key_states, value_states = past_key_values.update(key_states, value_states, self.layer_idx, cache_kwargs)
 
-        attention_interface: Callable = eager_attention_forward
-        if self.config._attn_implementation != "eager":
-            attention_interface = ALL_ATTENTION_FUNCTIONS[self.config._attn_implementation]
+            attention_interface: Callable = eager_attention_forward
+            if self.config._attn_implementation != "eager":
+                attention_interface = ALL_ATTENTION_FUNCTIONS[self.config._attn_implementation]
 
-        attn_output, attn_weights = attention_interface(
-            self,
-            query_states,
-            key_states,
-            value_states,
-            attention_mask,
-            dropout=0.0 if not self.training else self.attention_dropout,
-            scaling=self.scaling,
-            sliding_window=self.sliding_window,
-            s_aux=self.sinks.to(torch.bfloat16), # diff with Llama
-            **kwargs,
-        )
-        with torch.autocast(device_type = "cuda", dtype = torch.bfloat16):
+
+            attn_output, attn_weights = attention_interface(
+                self,
+                query_states,
+                key_states,
+                value_states,
+                attention_mask,
+                dropout=0.0 if not self.training else self.attention_dropout,
+                scaling=self.scaling,
+                sliding_window=self.sliding_window,
+                s_aux=self.sinks.to(torch.bfloat16), # diff with Llama
+                **kwargs,
+            )
             attn_output = attn_output.reshape(*input_shape, -1).contiguous()
             attn_output = self.o_proj(attn_output)
-        return attn_output, attn_weights
+            return attn_output, attn_weights
     pass
     patch_function(transformers.models.gpt_oss.modeling_gpt_oss.GptOssAttention, "forward", forward)
 pass
