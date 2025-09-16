4bf0cb2cf916360f6aa8e2b784cb41630eae52f5
Daniel Han <danielhanchen@gmail.com>
2025-09-15 23:43:04 -0700

    Update loss_utils.py

--- unsloth_zoo/loss_utils.py
+++ unsloth_zoo/loss_utils.py
@@ -310,7 +310,7 @@ def _unsloth_get_batch_samples(self, epoch_iterator, num_batches, device = None,
         except StopIteration:
             break
     pass
-    print([x["input_ids"] for x in batch_samples])
+    print([x["input_ids"].shape for x in batch_samples])
 
     # Get num_items_in_batch
     if has_kwargs and len(batch_samples) > 0 and "labels" in batch_samples[0]:
