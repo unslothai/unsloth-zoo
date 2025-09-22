eff4e09da07440c1546fa3d81923b736a1ff2205
Daniel Han <danielhanchen@gmail.com>
2025-09-22 04:20:55 -0700

    Update attention_sink.py

--- unsloth_zoo/flex_attention/attention_sink.py
+++ unsloth_zoo/flex_attention/attention_sink.py
@@ -216,30 +216,15 @@ def flex_attention_with_sink(
     if block_mask is None:
         block_mask = create_block_mask_cached(mask_mod, qlen_Q, qlen_KV, device = key.device)
 
-    # attn_output, logsumexp = (flex_attention if compile else uncompiled_flex_attention)(
-    #     query,
-    #     key,
-    #     value,
-    #     block_mask = block_mask,
-    #     score_mod = None, # None needed
-    #     enable_gqa = enable_gqa,
-    #     scale = scale,
-    #     return_lse = True, # log(sum(exp(xi)))
-    # )
-    attn_output, logsumexp, _, _, _ = torch.ops.aten._flash_attention_forward(
-        query.transpose(1, 2),
-        key.transpose(1, 2),
-        value.transpose(1, 2),
-        cum_seq_q = qlens,
-        cum_seq_k = qlens,
-        max_q = max_qlen,
-        max_k = max_qlen,
-        dropout_p = 0,
-        is_causal = True,
-        return_debug_mask = False,
+    attn_output, logsumexp = (flex_attention if compile else uncompiled_flex_attention)(
+        query,
+        key,
+        value,
+        block_mask = block_mask,
+        score_mod = None, # None needed
+        enable_gqa = enable_gqa,
         scale = scale,
-        window_size_left = sliding_window,
-        window_size_right = None,
+        return_lse = True, # log(sum(exp(xi)))
     )
 
     #### 3 versions to add sink tokens ####
@@ -264,6 +249,6 @@ def flex_attention_with_sink(
         sink_scale = torch.sigmoid(logsumexp, out = logsumexp)
         attn_output *= sink_scale.unsqueeze(-1).to(attn_output.dtype)
 
-    # attn_output = attn_output.transpose(1, 2).contiguous()
+    attn_output = attn_output.transpose(1, 2).contiguous()
     return attn_output
 pass
