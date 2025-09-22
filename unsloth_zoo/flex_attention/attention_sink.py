aee86d635f898c0fd8453f8d48f0dd9cc814cc0a
Daniel Han <danielhanchen@gmail.com>
2025-09-21 19:44:45 -0700

    Update attention_sink.py

--- unsloth_zoo/flex_attention/attention_sink.py
+++ unsloth_zoo/flex_attention/attention_sink.py
@@ -130,7 +130,7 @@ def flex_attention_with_sink(
     value,
     scale = None,
     sliding_window = None,
-    compile = False,
+    compile = True,
 ):
     """
     Allows one sink token to be attended to for full/sliding window attention
