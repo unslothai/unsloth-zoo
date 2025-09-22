7a788d7479d3614109612f8a4abe0f4476f81340
Daniel Han <danielhanchen@gmail.com>
2025-09-22 04:05:21 -0700

    Update attention_sink.py

--- unsloth_zoo/flex_attention/attention_sink.py
+++ unsloth_zoo/flex_attention/attention_sink.py
@@ -163,11 +163,11 @@ def flex_attention_with_sink(
     mask_mod = None
     block_mask = None
     has_flex_cache = hasattr(self_attn, "_flex_attention_cache")
-    global first_self_attn
-    if first_self_attn is None:
-        first_self_attn = self_attn
-    if self_attn == first_self_attn:
-        print(query.shape, key.shape)
+    # global first_self_attn
+    # if first_self_attn is None:
+    #     first_self_attn = self_attn
+    # if self_attn == first_self_attn:
+    #     print(query.shape, key.shape)
 
     # Handle inference and training
     if is_training or (
