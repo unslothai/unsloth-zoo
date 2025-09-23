c492b70ed176cf5c8fa85646a7eaba8dd8870e83
Daniel Han <danielhanchen@gmail.com>
2025-09-23 09:10:01 -0700

    Update gpt_oss.py

--- unsloth_zoo/temporary_patches/gpt_oss.py
+++ unsloth_zoo/temporary_patches/gpt_oss.py
@@ -572,6 +572,29 @@ def moe_forward_inference(self, hidden_states):
     return mixed.view(batch_size, -1, moe.hidden_size).to(hidden_states.dtype)
 pass
 
+@torch.compile(dynamic = None, fullgraph = True, options = fused_torch_compile_options)
+def moe_forward_inference_bf16(self, hidden_states):
+    router_scores, router_indices = self.router(hidden_states)
+    routing_weights = router_scores
+
+    batch_size = hidden_states.shape[0]
+    hidden_states = hidden_states.reshape(-1, self.hidden_size)
+    num_experts = routing_weights.shape[1]
+    hidden_states = hidden_states.repeat(num_experts, 1)
+    hidden_states = hidden_states.view(num_experts, -1, self.hidden_size)
+    gate_up = torch.bmm(hidden_states, self.gate_up_proj) + self.gate_up_proj_bias[..., None, :]
+    gate, up = gate_up[..., ::2], gate_up[..., 1::2]
+    gate = gate.clamp(min=None, max=self.limit)
+    up = up.clamp(min=-self.limit, max=self.limit)
+    glu = gate * torch.sigmoid(gate * self.alpha)
+    next_states = torch.bmm(((up + 1) * glu), self.down_proj)
+    next_states = next_states + self.down_proj_bias[..., None, :]
+    next_states = next_states.view(num_experts, batch_size, -1, self.hidden_size)
+    next_states = next_states * routing_weights.transpose(0, 1).view(num_experts, batch_size, -1)[..., None]
+    next_states = next_states.sum(dim=0)
+    return next_stats
+pass
+
 
 class GptOssMLP(nn.Module):
     def __init__(self, config):
@@ -1074,7 +1097,10 @@ def patch_GptOssModel():
                     logsumexp,
                     input_shape,
                 )
-                hidden_states = moe_forward_inference(decoder_layer.mlp, hidden_states)
+                if hasattr(decoder_layer.mlp, "gate_up_projs"):
+                    hidden_states = moe_forward_inference(decoder_layer.mlp, hidden_states)
+                else:
+                    hidden_states = moe_forward_inference_bf16(decoder_layer.mlp, hidden_states)
                 hidden_states += residual
             pass
             hidden_states = rms_layernorm_forward(self.norm, hidden_states)
