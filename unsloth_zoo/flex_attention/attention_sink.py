bc39493954329518ca8411b5fe2c5d4192831104
Daniel Han <danielhanchen@gmail.com>
2025-09-22 21:48:01 -0700

    Update attention_sink.py

--- unsloth_zoo/flex_attention/attention_sink.py
+++ unsloth_zoo/flex_attention/attention_sink.py
@@ -183,7 +183,7 @@ def flex_attention_with_sink(
             assert attention_mask.dim() == 2, f"Unsloth: Attention_mask has dim = {attention_mask.dim()}"
 
             padding_start_idx = attention_mask.argmax(1)
-            print(query, key)
+            print(query.shape, key.shape, padding_start_idx)
             # Use special padded mask creators
             mask_mod = prefill_mask_mod = \
                 generate_sliding_window_mask_with_padding(sliding_window, padding_start_idx) \
