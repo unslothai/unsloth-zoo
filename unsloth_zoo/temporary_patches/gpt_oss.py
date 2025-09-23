de35ca0f2bc423512adc09d8160137e1fdc7469c
Daniel Han <danielhanchen@gmail.com>
2025-09-23 09:14:45 -0700

    Update gpt_oss.py

--- unsloth_zoo/temporary_patches/gpt_oss.py
+++ unsloth_zoo/temporary_patches/gpt_oss.py
@@ -577,19 +577,20 @@ def moe_forward_inference_bf16(self, hidden_states):
     router_scores, router_indices = self.router(hidden_states)
     routing_weights = router_scores
 
+    moe = self.experts
     batch_size = hidden_states.shape[0]
-    hidden_states = hidden_states.reshape(-1, self.hidden_size)
+    hidden_states = hidden_states.reshape(-1, moe.hidden_size)
     num_experts = routing_weights.shape[1]
     hidden_states = hidden_states.repeat(num_experts, 1)
-    hidden_states = hidden_states.view(num_experts, -1, self.hidden_size)
-    gate_up = torch.bmm(hidden_states, self.gate_up_proj) + self.gate_up_proj_bias[..., None, :]
+    hidden_states = hidden_states.view(num_experts, -1, moe.hidden_size)
+    gate_up = torch.bmm(hidden_states, moe.gate_up_proj) + moe.gate_up_proj_bias[..., None, :]
     gate, up = gate_up[..., ::2], gate_up[..., 1::2]
-    gate = gate.clamp(min=None, max=self.limit)
-    up = up.clamp(min=-self.limit, max=self.limit)
-    glu = gate * torch.sigmoid(gate * self.alpha)
-    next_states = torch.bmm(((up + 1) * glu), self.down_proj)
-    next_states = next_states + self.down_proj_bias[..., None, :]
-    next_states = next_states.view(num_experts, batch_size, -1, self.hidden_size)
+    gate = gate.clamp(min=None, max=moe.limit)
+    up = up.clamp(min=-moe.limit, max=moe.limit)
+    glu = gate * torch.sigmoid(gate.to(torch.float32) * moe.alpha).to(gate.dtype)
+    next_states = torch.bmm(((up + 1) * glu), moe.down_proj)
+    next_states = next_states + moe.down_proj_bias[..., None, :]
+    next_states = next_states.view(num_experts, batch_size, -1, moe.hidden_size)
     next_states = next_states * routing_weights.transpose(0, 1).view(num_experts, batch_size, -1)[..., None]
     next_states = next_states.sum(dim=0)
     return next_stats
