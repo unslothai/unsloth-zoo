07c65c75b372027f3e3b6d00a4b2a43e4a68bc49
Daniel Han <danielhanchen@gmail.com>
2025-09-23 06:12:27 -0700

    Update gpt_oss.py

--- unsloth_zoo/temporary_patches/gpt_oss.py
+++ unsloth_zoo/temporary_patches/gpt_oss.py
@@ -553,8 +553,6 @@ def moe_forward_inference(self, hidden_states):
     X_rep = hidden_states.unsqueeze(0).expand(num_experts, -1, -1)
 
     # Gate up projection
-    print(X_rep.dtype)
-    print([up_l.bias.dtype for e, up_l in enumerate(moe.gate_up_projs)])
     gate_up_list = [up_l(X_rep[e]) for e, up_l in enumerate(moe.gate_up_projs)]
     gate_up = torch.stack(gate_up_list, dim = 0)
     dtype = torch.float32 if hidden_states.dtype != torch.bfloat16 else hidden_states.dtype
@@ -1027,6 +1025,7 @@ def patch_GptOssModel():
                     key_states,
                     value_states,
                 )
+                print(attn_output.dtype)
                 hidden_states = post_forward(
                     decoder_layer,
                     residual,
@@ -1034,6 +1033,7 @@ def patch_GptOssModel():
                     logsumexp,
                     input_shape,
                 )
+                print(hidden_states.dtype)
             pass
         pass
         hidden_states = self.norm(hidden_states)
