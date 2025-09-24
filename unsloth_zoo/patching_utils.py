2cebcc9f00e58dafb16384d8fa532ba494e87654
Daniel Han <danielhanchen@gmail.com>
2025-09-24 01:20:48 -0700

    Update patching_utils.py

--- unsloth_zoo/patching_utils.py
+++ unsloth_zoo/patching_utils.py
@@ -170,7 +170,7 @@ def patch_torch_compile(debug = False, O3 = False, ignore_errors = True):
         # TypeError: ValueRangeAnalysis.to_dtype() got an unexpected keyword argument 'use_compute_types'
         # this keyword exists in torch2.7.0 but not in torch2.6.0 so set to False until torch2.6.0 is deprecated.
         "config.emulate_precision_casts = False", # Force X.to(f32).to(f16) instead of X.to(f16)
-        "config.triton.tile_reductions = True", # Useful for GPT-OSS for Reduction over non-contiguous dims.
+        "config.triton.tile_reductions = False", # Useful for GPT-OSS for Reduction over non-contiguous dims.
         "config.triton.prefer_nd_tiling = False", # Used together with config.tile_reductions
     ]
     # Torch dynamo arguments
