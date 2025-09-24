6bd3f7017dad90c2415575d0f08c834fb3285949
Daniel Han <danielhanchen@gmail.com>
2025-09-24 01:54:15 -0700

    Update mxfp4.py

--- unsloth_zoo/temporary_patches/mxfp4.py
+++ unsloth_zoo/temporary_patches/mxfp4.py
@@ -20,13 +20,10 @@ import inspect
 import torch
 import torch.nn as nn
 import os
-import logging
 import math
 
-from .common import TEMPORARY_PATCHES, torch_compile_options, UNSLOTH_ENABLE_LOGGING
-
-logger = logging.getLogger(__name__)
-
+from .common import TEMPORARY_PATCHES, torch_compile_options
+from .utils import patch_function
 
 def patch_convert_moe_packed_tensors():
     """
@@ -94,16 +91,7 @@ def patch_convert_moe_packed_tensors():
         out = out.reshape(*prefix_shape, G, B * 2).view(*prefix_shape, G * B * 2)
         del blocks, scales, lut
         return out
-
-    # Add the new CPU function to the mxfp4 module
-    if hasattr(transformers.integrations.mxfp4, 'convert_moe_packed_tensors'):
-        transformers.integrations.mxfp4.convert_moe_packed_tensors = convert_moe_packed_tensors
-        if UNSLOTH_ENABLE_LOGGING:
-            print("Unsloth: Successfully added convert_moe_packed_tensors_cpu function.")
-    else:
-        if UNSLOTH_ENABLE_LOGGING:
-            print("Unsloth: Failed to add convert_moe_packed_tensors_cpu - original function not found.")
-    return
+    patch_function(transformers.integrations.mxfp4, "convert_moe_packed_tensors", convert_moe_packed_tensors)
 pass
 TEMPORARY_PATCHES.append(patch_convert_moe_packed_tensors)
 
@@ -186,10 +174,61 @@ def patch_convert_moe_packed_tensors_cpu():
     if hasattr(transformers.integrations.mxfp4, 'convert_moe_packed_tensors'):
         transformers.integrations.mxfp4.convert_moe_packed_tensors_cpu = convert_moe_packed_tensors_cpu
         if UNSLOTH_ENABLE_LOGGING:
-            print("Unsloth: Successfully added convert_moe_packed_tensors_cpu function.")
+            logger.info("Unsloth: Successfully added convert_moe_packed_tensors_cpu function.")
     else:
         if UNSLOTH_ENABLE_LOGGING:
-            print("Unsloth: Failed to add convert_moe_packed_tensors_cpu - original function not found.")
+            logger.info("Unsloth: Failed to add convert_moe_packed_tensors_cpu - original function not found.")
     return
 pass
 TEMPORARY_PATCHES.append(patch_convert_moe_packed_tensors_cpu)
+
+
+def patch_mxfp4_dequantize():
+    """
+    Transformers 4.55.4 did dequantized.transpose(1, 2).contiguous().to(target_device)
+    but new versions > 4.56.0 removed the transpose(1, 2) and moved it into patch_convert_moe_packed_tensors
+    """
+    try:
+        import transformers.integrations.mxfp4
+        from transformers.integrations.tensor_parallel import shard_and_distribute_module
+    except:
+        return
+    def dequantize(module, param_name, param_value, target_device, dq_param_name, **kwargs):
+
+        model = kwargs.get("model", None)
+        empty_param = kwargs.get("empty_param", None)
+        casting_dtype = kwargs.get("casting_dtype", None)
+        to_contiguous = kwargs.get("to_contiguous", None)
+        rank = kwargs.get("rank", None)
+        device_mesh = kwargs.get("device_mesh", None)
+
+        for proj in ["gate_up_proj", "down_proj"]:
+            if proj in param_name:
+                if device_mesh is not None:
+                    param_value = shard_and_distribute_module(
+                        model,
+                        param_value,
+                        empty_param,
+                        dq_param_name,
+                        casting_dtype,
+                        to_contiguous,
+                        rank,
+                        device_mesh,
+                        set_param=False,
+                    )
+                blocks_attr = f"{proj}_blocks"
+                scales_attr = f"{proj}_scales"
+                setattr(module, param_name.rsplit(".", 1)[1], param_value)
+                if hasattr(module, blocks_attr) and hasattr(module, scales_attr):
+                    dequantized = convert_moe_packed_tensors(getattr(module, blocks_attr), getattr(module, scales_attr))
+                    # [HERE] we must do transpose(1, 2)
+                    dequantized = dequantized.transpose(1, 2).contiguous().to(target_device)
+                    # TODO: this is perhaps necessary since if target_device is cpu, and the param was on gpu
+                    if target_device == "cpu" and torch.cuda.is_available():
+                        torch.cuda.empty_cache()
+                    setattr(module, proj, torch.nn.Parameter(dequantized))
+                    delattr(module, blocks_attr)
+                    delattr(module, scales_attr)
+    patch_function(transformers.integrations.mxfp4, "dequantize", dequantize)
+pass
+TEMPORARY_PATCHES.append(patch_mxfp4_dequantize)
