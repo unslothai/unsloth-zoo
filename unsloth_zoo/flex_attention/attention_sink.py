4e57162b6431640fb16f057fb923eb60b8cc1183
Daniel Han <danielhanchen@gmail.com>
2025-09-25 04:58:26 -0700

    Update attention_sink.py

--- unsloth_zoo/flex_attention/attention_sink.py
+++ unsloth_zoo/flex_attention/attention_sink.py
@@ -198,23 +198,23 @@ def flex_attention_with_sink(
                 assert attention_mask.dim() == 2, f"Unsloth: Attention_mask has dim = {attention_mask.dim()}"
                 # We must account for left padding
                 padding_start_idx = attention_mask.argmax(1).to(query.device)
-                # do_padding = torch.arange(max(qlen_Q, qlen_KV), device = query.device).repeat((bsz, 1)) < padding_start_idx.unsqueeze(0).T
+                do_padding = torch.arange(max(qlen_Q, qlen_KV), device = query.device).repeat((bsz, 1)) < padding_start_idx.unsqueeze(0).T
                 # We also make all padded tokens Q=1, K=-inf
                 # Note if Q=0, K=0, Q*K = 0, but exp(0) = 1, so that's wrong
                 # Only exp(-inf) = 0. So Q=1, K=-inf, Q*K = -inf
-                # query.transpose(2, 1)[do_padding[:, :qlen_Q ]] = 1
-                # key  .transpose(2, 1)[do_padding[:, :qlen_KV]] = -torch.inf
-                # value.transpose(2, 1)[do_padding[:, :qlen_KV]] = 0
+                query.transpose(2, 1)[do_padding[:, :qlen_Q ]] = 1
+                key  .transpose(2, 1)[do_padding[:, :qlen_KV]] = -torch.inf
+                value.transpose(2, 1)[do_padding[:, :qlen_KV]] = 0
                 # Use special padded mask creators
                 mask_mod = prefill_mask_mod = \
                     generate_sliding_window_mask_with_padding(sliding_window, padding_start_idx) \
                     if type(sliding_window) is int and sliding_window != 0 else \
                     generate_causal_mask_with_padding(padding_start_idx)
-                # decoding_mask_mod = \
-                #     generate_decoding_sliding_window_mask_with_padding(sliding_window, padding_start_idx) \
-                #     if type(sliding_window) is int and sliding_window != 0 else \
-                #     generate_decoding_causal_mask_with_padding(padding_start_idx)
-                # self_attn._flex_attention_cache = FlexAttentionCache(key, decoding_mask_mod, sliding_window)
+                decoding_mask_mod = \
+                    generate_decoding_sliding_window_mask_with_padding(sliding_window, padding_start_idx) \
+                    if type(sliding_window) is int and sliding_window != 0 else \
+                    generate_decoding_causal_mask_with_padding(padding_start_idx)
+                self_attn._flex_attention_cache = FlexAttentionCache(key, decoding_mask_mod, sliding_window)
         else:
             block_mask = self_attn._flex_attention_cache(key)
         pass
