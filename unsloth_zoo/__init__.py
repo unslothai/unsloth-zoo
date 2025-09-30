d241d8d8fa117eec1b80428ce0e5395c1a119687
Daniel Han <danielhanchen@gmail.com>
2025-09-30 04:35:13 -0700

    Versioning

--- unsloth_zoo/__init__.py
+++ unsloth_zoo/__init__.py
@@ -14,7 +14,7 @@
 # You should have received a copy of the GNU Lesser General Public License
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-__version__ = "2025.9.12"
+__version__ = "2025.9.13"
 
 import os
 # Hugging Face Hub faster downloads
