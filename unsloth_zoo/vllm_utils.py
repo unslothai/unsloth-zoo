73613a4ce162b4475a9b64dafd9a165754bfd9a2
Datta Nimmaturi <venkatadattasainimmaturi@gmail.com>
2025-10-23 10:08:41 +0000

    Use patched FP8 methods

--- unsloth_zoo/vllm_utils.py
+++ unsloth_zoo/vllm_utils.py
@@ -1124,14 +1124,14 @@ def convert_vllm_to_huggingface(quant_state_dict, config, dtype = torch.float16,
                 kwargs['activation_scheme'] = quantization_config['activation_scheme']
                 kwargs['block_size'] = quantization_config['weight_block_size']
                 try:
-                    from transformers.integrations.finegrained_fp8 import FP8Linear
+                    from unsloth.kernels.fp8 import FP8Linear # This has patched forward pass for LoRA and training support
                 except ImportError:
                     raise ImportError("Unsloth: FP8 models need importing FP8Linear from `transformers.integrations.finegrained_fp8` but we don't see it.")
 
             elif quant_method == 'fbgemm_fp8':
                 kwargs['input_scale_ub'] = torch.tensor([quantization_config['activation_scale_ub']], device = get_target_device())
                 try:
-                    from transformers.integrations.fbgemm_fp8 import FbgemmFp8Linear
+                    from unsloth.kernels.fp8 import FbgemmFp8Linear  # This has patched forward pass for LoRA and training support
                 except ImportError:
                     raise ImportError("Unsloth: FP8 models need importing FbgemmFP8Linear from `transformers.integrations.fbgemm_fp8` but we don't see it.")
 
