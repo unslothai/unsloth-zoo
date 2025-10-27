3979ea0ce8e33a6785cbc60c585a39fd3418f3a3
Daniel Han <danielhanchen@gmail.com>
2025-10-26 21:34:47 -0700

    Fix cut cross entropy

--- unsloth_zoo/__init__.py
+++ unsloth_zoo/__init__.py
@@ -14,7 +14,7 @@
 # You should have received a copy of the GNU Lesser General Public License
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-__version__ = "2025.10.10"
+__version__ = "2025.10.11"
 
 import os
 import warnings
