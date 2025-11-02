858b894dcab21d108160cdd90a3b34c47004b28f
pluesclues <136766175+pluesclues@users.noreply.github.com>
2025-11-02 17:44:00 -0500

    Refactor importance sampling logic in rl_replacements.py, and put into no grad

--- unsloth_zoo/rl_replacements.py
+++ unsloth_zoo/rl_replacements.py
@@ -250,15 +250,15 @@ def grpo_compute_loss(
             old_x = torch.gather(old_logits, dim = -1, index = input_ids).squeeze(-1)
             old = old_x - torch.logsumexp(old_logits, dim = -1)
         pass
+        if use_vllm and sampling_per_token_logps is not None:
+            #must filter out extra prompt tokens in begining after making input_ids left padded
+            importance_sampling_ratio = torch.exp((old * mask) - sampling_per_token_logps)
+            importance_sampling_ratio = torch.clamp(
+                importance_sampling_ratio, max=vllm_importance_sampling_cap
+            )
+        pass
     pass
-
-    if use_vllm and sampling_per_token_logps is not None:
-        #must filter out extra prompt tokens in begining after making input_ids left padded
-        importance_sampling_ratio = torch.exp((old * mask) - sampling_per_token_logps)
-        importance_sampling_ratio = torch.clamp(
-            importance_sampling_ratio, max=vllm_importance_sampling_cap
-        )
-
+    
     # Reverse KL
     # Note that this is a low variance low bias estimator for the KL divergence as used in GRPO paper
     if beta != 0.0:
