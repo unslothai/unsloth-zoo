3e48e6100de3777f7edc08ce3868bf7985b6b6a8
Daniel Han <danielhanchen@gmail.com>
2025-12-08 04:10:14 -0800

    Update __init__.py

--- unsloth_zoo/__init__.py
+++ unsloth_zoo/__init__.py
@@ -14,7 +14,7 @@
 # You should have received a copy of the GNU Lesser General Public License
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-__version__ = "2025.12.1"
+__version__ = "2025.12.2"
 
 import os
 import warnings
