e32b17e7ca17ee6bbd17356de1a96b6fc6735ff7
pluesclues <136766175+pluesclues@users.noreply.github.com>
2025-12-10 12:02:21 -0500

    Add torch.cuda.synchronize() for race condition handling
    Added synchronization to avoid race conditions with GPT OSS offload.

--- unsloth_zoo/rl_replacements.py
+++ unsloth_zoo/rl_replacements.py
@@ -707,7 +707,9 @@ def grpo_accumulated_loss(
                 logit_softcapping = logit_softcapping, 
                 temperature = temperature
             )
-            
+            #This is needed to avoid race conditions with GPT OSS offload_embbed=True
+            #However, it seems that this line does not slow down or disrupt models. 
+            torch.cuda.synchronize()
             all_logprobs_list.append(logprobs_chunk)
 
         new_logprobs = torch.cat(all_logprobs_list, dim=0)
