be54ecc375904ee40b6513bece96502af49e527e
Datta Nimmaturi <venkatadattasainimmaturi@gmail.com>
2026-02-16 13:50:30 +0000

    Qwen3.5 MoE

--- unsloth_zoo/temporary_patches/__init__.py
+++ unsloth_zoo/temporary_patches/__init__.py
@@ -23,6 +23,7 @@ from .gpt_oss import *
 from .qwen3_moe import *
 from .qwen3_vl_moe import *
 from .qwen3_next_moe import *
+from .qwen3_5_moe import *
 from .glm4_moe import *
 from .deepseek_v3_moe import *
 from .pixtral import *
