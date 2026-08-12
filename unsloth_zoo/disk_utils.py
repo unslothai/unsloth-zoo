85ff9aabd358c530d1518dd724ae5302ca85554e
Daniel Han <danielhanchen@gmail.com>
2026-08-12 12:24:04 +0000

    License the two new disk guard files under AGPL-3.0 like the rest of the new zoo sources

--- unsloth_zoo/disk_utils.py
+++ unsloth_zoo/disk_utils.py
@@ -1,18 +1,19 @@
-# Unsloth Zoo - Utils for Unsloth
+# Unsloth Zoo - Utilities for Unsloth
 # Copyright 2023-present Daniel Han-Chen, Michael Han-Chen & the Unsloth team. All rights reserved.
 #
 # This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU Lesser General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
+# it under the terms of the GNU Affero General Public License as published
+# by the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
 #
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU Lesser General Public License for more details.
+# GNU Affero General Public License for more details.
 #
-# You should have received a copy of the GNU Lesser General Public License
+# You should have received a copy of the GNU Affero General Public License
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 """Disk sizing helpers, plus the one place that decides "are we on Kaggle?".
 
 Two separate problems live here because they are the same problem seen twice:
