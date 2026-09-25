67a6eb69ca03bb32288b5ac73859cbeccdb06410
Daniel Han <23090290+danielhanchen@users.noreply.github.com>
2026-09-25 14:08:24 +0000

    Use the AGPL header on the new MoE kernel module and tests

--- unsloth_zoo/temporary_patches/moe_triton_kernels.py
+++ unsloth_zoo/temporary_patches/moe_triton_kernels.py
@@ -1,16 +1,17 @@
-# Copyright 2023-present Daniel Han-Chen & the Unsloth team. All rights reserved.
+# Unsloth Zoo - Utilities for Unsloth
+# Copyright 2023-present Daniel Han-Chen, Michael Han-Chen & the Unsloth team. All rights reserved.
 #
 # This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU Lesser General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
+# it under the terms of the GNU Affero General Public License as published
+# by the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
 #
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU Lesser General Public License for more details.
+# GNU Affero General Public License for more details.
 #
-# You should have received a copy of the GNU Lesser General Public License
+# You should have received a copy of the GNU Affero General Public License
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 """Triton MoE kernels (NF4 dequant, weighted unpermute), bit-identical to the eager
 paths they replace. Launched with enable_fp_fusion=False because the eager paths round
